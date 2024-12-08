cd280679fae5e1562aa76bbffa9d85296675057c
ImdiMelvana <imdimelvana@gmail.com>
2024-12-08 15:15:18 +0100

    refactor the code

--- Cluster_Sckitlearn.py
+++ Cluster_Sckitlearn.py
@@ -62,7 +62,7 @@ y_min, y_max = X2[:, 1].min() - 1, X2[:, 1].max() + 1
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
 Z2 = algorithm.predict(np.c_[xx.ravel(), yy.ravel()])
 
-plt.figure(1 , figsize = (15 , 7) )
+plt.figure(2 , figsize = (15 , 7) )
 plt.clf()
 Z2 = Z2.reshape(xx.shape)
 plt.imshow(Z2 , interpolation='nearest', 
@@ -75,41 +75,25 @@ plt.scatter(x = centroids2[: , 0] , y =  centroids2[: , 1] , s = 300 , c = 'red'
 plt.ylabel('Spending Score (1-100)') , plt.xlabel('Annual Income (k$)')
 plt.show()
 
-"3.Segmentation using Age , Annual Income and Spending Score"
-X3 = df[['Age' , 'Annual Income (k$)' ,'Spending Score (1-100)']].iloc[: , :].values
+# 3. Segmentation using Age, Annual Income and Spending Score
+X3 = df[['Age', 'Annual Income (k$)', 'Spending Score (1-100)']].values
 
-# Removed the elbow graph section for X3
-
-algorithm = (KMeans(n_clusters = 6 ,init='k-means++', n_init = 10 ,max_iter=300, 
-                        tol=0.0001,  random_state= 111  , algorithm='elkan') )
+# Performing KMeans clustering with optimal clusters
+algorithm = KMeans(n_clusters=6, init='k-means++', n_init=10, max_iter=300, 
+                   tol=0.0001, random_state=111, algorithm='elkan')
 algorithm.fit(X3)
+
+# Extracting labels and centroids
 labels3 = algorithm.labels_
 centroids3 = algorithm.cluster_centers_
 
-df['label3'] =  labels3
-trace1 = go.Scatter3d(
-    x= df['Age'],
-    y= df['Spending Score (1-100)'],
-    z= df['Annual Income (k$)'],
-    mode='markers',
-     marker=dict(
-        color = df['label3'], 
-        size= 20,
-        line=dict(
-            color= df['label3'],
-            width= 12
-        ),
-        opacity=0.8
-     )
-)
-data = [trace1]
-layout = go.Layout(
-    title= 'Clusters',
-    scene = dict(
-            xaxis = dict(title  = 'Age'),
-            yaxis = dict(title  = 'Spending Score'),
-            zaxis = dict(title  = 'Annual Income')
-        )
-)
-fig = go.Figure(data=data, layout=layout)
-py.offline.iplot(fig)
+# Visualizing the 3D clustering result
+fig = plt.figure(3, figsize=(10, 8))
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(X3[:, 0], X3[:, 1], X3[:, 2], c=labels3, s=200, cmap='Pastel2')
+ax.scatter(centroids3[:, 0], centroids3[:, 1], centroids3[:, 2], s=300, c='red', alpha=0.5)
+ax.set_xlabel('Age')
+ax.set_ylabel('Annual Income (k$)')
+ax.set_zlabel('Spending Score (1-100)')
+ax.set_title('3D Clustering by Age, Annual Income and Spending Score')
+plt.show()
--- Cluster_tensorflow.py
+++ Cluster_tensorflow.py
@@ -38,7 +38,7 @@ optimal_clusters = 4  # Change this value based on the elbow plot
 labels1, centroids1 = kmeans_tf(X1, n_clusters=optimal_clusters)
 
 # Visualizing the clustering result
-plt.figure(figsize=(15, 7))
+plt.figure(1, figsize=(15, 7))
 plt.scatter(X1[:, 0], X1[:, 1], c=labels1, s=200, cmap='Pastel2')
 plt.scatter(centroids1[:, 0], centroids1[:, 1], s=300, c='red', alpha=0.5)
 plt.xlabel('Age')
@@ -53,7 +53,7 @@ X2 = df[['Annual Income (k$)', 'Spending Score (1-100)']].values
 labels2, centroids2 = kmeans_tf(X2, n_clusters=5)
 
 # Visualizing the clustering result
-plt.figure(figsize=(15, 7))
+plt.figure(2, figsize=(15, 7))
 plt.scatter(X2[:, 0], X2[:, 1], c=labels2, s=200, cmap='Pastel2')
 plt.scatter(centroids2[:, 0], centroids2[:, 1], s=300, c='red', alpha=0.5)
 plt.xlabel('Annual Income (k$)')
@@ -68,7 +68,7 @@ X3 = df[['Age', 'Annual Income (k$)', 'Spending Score (1-100)']].values
 labels3, centroids3 = kmeans_tf(X3, n_clusters=6)
 
 # Visualizing the 3D clustering result
-fig = plt.figure(figsize=(10, 8))
+fig = plt.figure(3, figsize=(10, 8))
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(X3[:, 0], X3[:, 1], X3[:, 2], c=labels3, s=200, cmap='Pastel2')
 ax.scatter(centroids3[:, 0], centroids3[:, 1], centroids3[:, 2], s=300, c='red', alpha=0.5)
